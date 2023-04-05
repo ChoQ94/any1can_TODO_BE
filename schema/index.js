5641f0e0c8c10f1fdb42d1b786c0759049118113
ChoQ94 <whrbdnjs1994@gmail.com>
2023-04-05 09:37:38 +0900

    feat: 파일명 정리

--- schema/index.js
+++ schema/index.js
@@ -2,19 +2,16 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const connect = () => {
   mongoose.connect(process.env.MONGO_DB);
-  // 3. 연결된 testDB 사용
   var db = mongoose.connection;
-  // 4. 연결 실패
 
   db.on("error", function () {
     console.log("Connection Failed!");
   });
-  // 5. 연결 성공
   db.once("open", function () {
     console.log("Connected!");
   });
 };
-
+// 설정을 추가할 수 있는 방식
 // mongoose
 //   .connect(process.env.MONGO_DB, {
 //     useNewUrlParser: true,
